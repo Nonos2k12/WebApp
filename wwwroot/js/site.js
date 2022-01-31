1b869b29fd8ae568093671b407bf73bcfb1f7111
Nonos2k12 <arnaud.jouanne89@gmail.com>
2022-01-31 16:27:20 +0100

    Deleted unused JS code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,37 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-/*$(document).ready(function () {
-    $("#contact").click(function () {
-        if ($(this).val() == "email") {
-            $("#email").show();
-        }
-        else if ($(this).val() == "telephone") {
-            $("#telephone").show();
-        }
-        else {
-            $("#email").hide();
-            $("#telephone").hide();
-        }
-    });
-});
-$(document).ready(function () {
-    $('#contactChoice1').click(function () {
-        //$('#email').show();
-        $('#email').css('visibility', 'visible');
-    });
-    $('#contactChoice2').click(function () {
-        //$('#telephone').show();
-        $('#telephone').css('visibility', 'visible');
-    });
-});
-
-function showInputEmail() {
-    $('#email').css('visibility', 'visible');
-}*/
-
+﻿// Fonction pour afficher ou non les champs email et téléphone du formulaire
 $(document).ready(function () {
     $('#txtDemande').on('change', function () {
         if (this.value == 'Téléphone') {
@@ -64,18 +31,6 @@ let saveFile = () => {
     let data = "Nom: " + nom.value + " \r\n " + "Prenom: " + prenom.value + " \r\n " + "Email: " + email.value + " \r\n " + "Telephone: " + telephone.value + " \r\n " + "Société: " + societe.value + " \r\n " + "Type de demande: " + demande.value + " \r\n " + "Date: " + date.value + " \r\n " + "UserId: " + userId.value;
     // Convert the text to BLOB.
     const textToBLOB = new Blob([data], { type: "text/plain" });
-    //var filename = new Date();
-    /*var month = new Date(); //months from 1-12
-    month = month.getMonth();
-
-    var day = new Date();
-    var day = day.getUTCDate();
-
-    var year = new Date();
-    var year = year.getUTCFullYear();*/
-
-    //newdate = year + "/" + month + "/" + day;
-    //const sFileName = filename; // The file to save the data.
 
     let newLink = document.createElement("a");
     newLink.download = societe.value + " " + date.value;
@@ -87,6 +42,5 @@ let saveFile = () => {
         newLink.style.display = "none";
         document.body.appendChild(newLink);
     }
-
     newLink.click();
 };
